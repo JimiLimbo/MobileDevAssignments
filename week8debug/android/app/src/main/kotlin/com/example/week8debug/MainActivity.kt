7bfdcf1ade3fa5fcadcfe020a5e380ee4e3e1aa6
Seth Caster <111807881+JimiLimbo@users.noreply.github.com>
2023-10-10 21:20:20 -0500

    Did, done, deal

---
+++ week8debug/android/app/src/main/kotlin/com/example/week8debug/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.week8debug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ week8debug/lib/BusinessLogic/TaxCalculation.dart
@@ -0,0 +1,32 @@
+import '../Models/Item.dart';
+
+class TaxCalculation {
+  ///Calculates item royalty based on a few item properties
+  double CalculateItemRoyalty(Item item) {
+    double royalty = 0.0;
+
+    try {
+      if (item.Name.toLowerCase().contains("nokia")) {
+        royalty += 10;
+      }
+      if (item.Description.toLowerCase().contains("3310")) {
+        royalty += 5.5;
+      }
+      if (item.Price >= 50.0 && item.Price <= 100.0) {
+        royalty += 30;
+      }
+      if (item.Name.toLowerCase().contains("nokia") &&
+          item.Description.toLowerCase().contains("3310")) {
+        royalty += 7.0;
+      }
+      double tax = .05;
+      if (royalty > 1.0) {
+        royalty += item.Price * tax;
+      }
+
+      return royalty;
+    } catch (e) {
+      return 10.00;
+    }
+  }
+}
